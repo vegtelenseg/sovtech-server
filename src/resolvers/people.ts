3909caa0f922889414515cd921250d709ab99501
Siya Mzam <siya@stackworx.io>
2021-04-05 15:45:12 +0200

    resolve mass field manually

--- src/resolvers/people.ts
+++ src/resolvers/people.ts
@@ -27,6 +27,8 @@ export default (fetch: IFetcher) => ({
     skinColor: (person) => person.skin_color,
     eyeColor: (person) => person.eye_color,
     birthYear: (person) => person.birth_year,
+    // Can be null, so have to resolve manually
+    mass: (person) => (person.mass ? person.mass : 0),
     homeworld: (person, _, context) => context.loader.load(person.homeworld),
     films: (person, _, context) => context.loader.loadMany(person.films),
     species: (person, _, context) => context.loader.loadMany(person.species),
